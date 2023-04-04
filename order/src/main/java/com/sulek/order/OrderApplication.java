e30803c76b05117b283ae8daa1455cd1361726fd
Anıl Şülekoğlu <39844117+sulek1201@users.noreply.github.com>
2023-04-04 18:11:56 +0300

    online-order-system initiated

---
+++ order/src/main/java/com/sulek/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.sulek.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
